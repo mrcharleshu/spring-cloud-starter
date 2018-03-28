af5b8689a3b0d3a5fe73191ca0a2602b8a7bc89e
Charles <mrcharleshu@gmail.com>
2018-03-28 11:57:35 +0800

    添加sharding-jdbc-mybatis

--- scs-supplier-db/scs-supplier-02/src/main/java/com/charles/springcloud/supplier/db/sharding/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.charles.springcloud.supplier.db.sharding;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
-}
--- scs-supplier-db/scs-supplier-02/src/main/java/com/charles/springcloud/supplier/db/sharding/OrderService.java
+++
@@ -1,5 +0,0 @@
-package com.charles.springcloud.supplier.db.sharding;
-
-public interface OrderService {
-    Order findOne(Long orderId);
-}
