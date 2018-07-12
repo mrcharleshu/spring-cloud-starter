e00b4f6d1344ed6f08216cdfa2be4081be8003d1
hubin <mrcharleshu@gmail.com>
2018-07-12 10:24:07 +0800

    升级springclound的版本

--- scs-consumer-feign/src/main/java/com/charles/springcloud/FeignApplication.java
+++ scs-consumer-feign/src/main/java/com/charles/springcloud/FeignApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,16 +15,23 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableFeignClients
 @EnableHystrix
 @ComponentScan("com.charles.springcloud")
-public class Application {
+public class FeignApplication {
     @Bean//定义REST客户端，RestTemplate实例
     @LoadBalanced//开启负债均衡的能力
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
+    //@Bean
+    //@Scope("prototype")
+    //public Feign.Builder feignBuilder() {
+    //    return Feign.builder();
+    //}
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(FeignApplication.class, args);
     }
 }
--- scs-consumer-ribbon/src/main/java/com/charles/springcloud/RibbonApplication.java
+++ scs-consumer-ribbon/src/main/java/com/charles/springcloud/RibbonApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,23 +14,16 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-@EnableFeignClients
 @EnableHystrix
 @ComponentScan("com.charles.springcloud")
-public class Application {
+public class RibbonApplication {
     @Bean//定义REST客户端，RestTemplate实例
     @LoadBalanced//开启负债均衡的能力
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
-    //@Bean
-    //@Scope("prototype")
-    //public Feign.Builder feignBuilder() {
-    //    return Feign.builder();
-    //}
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(RibbonApplication.class, args);
     }
 }
